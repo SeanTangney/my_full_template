0e94686cc3a31633d4a29575e127f52a51c2d3e9
SeanTangney <sean_tangney@hotmail.com>
2021-12-08 14:40:03 +0000

    make computer choice function and game choice function. Check if they will both log to the console

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,13 +10,34 @@ const rock_div = document.getElementById("rock");
 const paper_div = document.getElementById("paper");
 const scissors_div = document.getElementById("scissors");
 
+/**
+ * Generate random number to to give us a random element from the array
+ */
+function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+}
+
+
+
+function game(userChoice){
+    const computerChoice = getComputerChoice();
+}
+
+
+/**
+ * Event listeners to determine the users choice ( rock, paper or scissors)
+ */
+function main(){
 rock_div.addEventListener('click', function(){
-    console.log("You chose rock");
+    game("rock");
 })
 paper_div.addEventListener('click', function(){
-    console.log("You chose paper");
+    game("paper");
 })
 scissors_div.addEventListener('click', function(){
-    console.log("You chose scissors");
+    game("scissors");
 })
-
+}
+ main();
