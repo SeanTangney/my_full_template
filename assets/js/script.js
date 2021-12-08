2bb83638c0ac00dc40b66cbb19073ebdbf480b06
SeanTangney <sean_tangney@hotmail.com>
2021-12-08 11:41:40 +0000

    Add the tree 'weapons' into a div in the game.html page. add event listeners in the script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,22 @@
-/*Scoreboard variables*/
+/*Caching the dom*/
 
-let userScore = 0;
-let computerScore = 0;
-let userScore_span = document.getElementById("user-score");
-let computerScore_span = document.getElementById("computer-score");
+const userScore = 0;
+const computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".scoreboard");
+const result_div = document.querySelector(".game-result");
+const rock_div = document.getElementById("rock");
+const paper_div = document.getElementById("paper");
+const scissors_div = document.getElementById("scissors");
+
+rock_div.addEventListener('click', function(){
+    console.log("You chose rock");
+})
+paper_div.addEventListener('click', function(){
+    console.log("You chose paper");
+})
+scissors_div.addEventListener('click', function(){
+    console.log("You chose scissors");
+})
 
