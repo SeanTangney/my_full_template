ff39d41fc6345fd2006a0b92480f2d86300e92b0
SeanTangney <sean_tangney@hotmail.com>
2021-12-08 20:47:41 +0000

    Added query selector for the result text that displays how you won or how you lost, or draw

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,11 +9,12 @@ const result_div = document.querySelector(".game-result");
 const rock_div = document.getElementById("rock");
 const paper_div = document.getElementById("paper");
 const scissors_div = document.getElementById("scissors");
+const result_text = document.querySelector(".result-text");
 
 /**
  * Generate random number to to give us a random element from the array
  */
-function getComputerChoice() {
+function getComputerChoice(userChoice, computerChoice) {
     const choices = ['rock', 'paper', 'scissors'];
     const randomNumber = Math.floor(Math.random() * 3);
     return choices[randomNumber];
@@ -22,33 +23,33 @@ function getComputerChoice() {
  * This Function executes when you win. It adds 1 to the user score. Displays +1 to the user score on the 
  * scoreboard and logs it to the console.
  */
-function win(user, computer){
+function win(userChoice, computerChoice){
     userScore++;
     userScore_span.innerHTML = userScore;
     computerScore_span.innerHTML = computerScore;
     console.log("win");
-    console.log(user);
-    console.log(computer);
+    computerScore_span.innerHTML = computerScore;
+    result_text.innerHTML = userChoice + " beats " + computerChoice;
 }
 /**
  * This function executes when you lose. It adds 1 to the computer score. Displays +1 to the computer score on the 
  * scoreboard and logs it to the console.
  */
-function lose(){
+function lose(userChoice, computerChoice){
     computerScore++;
     userScore_span.innerHTML = userScore;
     computerScore_span.innerHTML = computerScore;
     console.log("lose");
-    console.log(user);
-    console.log(computer);
+    computerScore_span.innerHTML = computerScore;
+    result_text.innerHTML = userChoice + " loses to " + computerChoice;
 }
 /**
  * This function executes 
  */
-function draw(){
+function draw(userChoice, computerChoice){
     console.log("draw");
-    console.log(user);
-    console.log(computer);
+    computerScore_span.innerHTML = computerScore;
+    result_text.innerHTML = userChoice + " was chosen by both, DRAW";
 }
         
 
@@ -65,17 +66,17 @@ function game(userChoice){
         case "rockscissors":
         case "paperrock":
         case "scissorspaper":
-            win(user, computer);
+            win(userChoice, computerChoice);
             break;
         case "rockpaper":
         case "paperscissors":
         case "scissorsrock":
-            lose(user, computer);
+            lose(userChoice, computerChoice);
             break;
         case "rockrock":
         case "paperpaper":
         case "scissorsscissors":
-            draw(user, computer);
+            draw(userChoice, computerChoice);
             break;            
     }
 }
